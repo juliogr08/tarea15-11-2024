fafad1562939bb40d1b80d642bd47809b15ebc9f
T403-16 <T403-16@DESKTOP-D9IF75B>
2024-11-15 10:36:37 -0400

    Agregar archivos de proyecto.

---
+++ tarea/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tarea.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
